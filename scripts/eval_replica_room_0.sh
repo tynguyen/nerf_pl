4426370289c9e60093a7d4722731270cbcd6d50c
Ty Nguyen <t.nguyen5@partner.samsung.com>
2021-07-17 23:31:47 -0400

    Add Replica training

--- scripts/eval_replica_room_0.sh
+++ scripts/eval_replica_room_0.sh
@@ -1,5 +1,4 @@
 python eval.py \
    --root_dir data/replica/room_0\
    --dataset_name llff --scene_name room_0 \
-   --use_disp \
-   --img_wh 504 378 --N_importance 64 --ckpt_path 'ckpts/exp_replica_room_0/epoch=09.ckpt' \
+   --img_wh 640 360 --N_importance 64 --ckpt_path 'ckpts/exp_replica_room_0/epoch=2.ckpt' \
--- scripts/train_replica_room_0.sh
+++ scripts/train_replica_room_0.sh
@@ -1,16 +1,17 @@
 ROOT_DIR="data/replica/room_0"
 # directory containing the data
-IMG_W=1280  # image width (do not set too large)
-IMG_H=720  # image height (do not set too large)
+IMG_W=640  # image width (do not set too large)
+IMG_H=360  # image height (do not set too large)
 NUM_EPOCHS=30  # number of epochs to train (depending on how many images there are,
 # 20~30 might be enough)
-EXP="exp_replica_room_0"  # name of the experience (arbitrary)
+EXP="exp_replica_room_0_spheric"  # name of the experience (arbitrary)
 
 python3 train.py \
    --dataset_name llff \
    --root_dir "$ROOT_DIR" \
-   --N_importance 64 --img_wh $IMG_W $IMG_H \
-   --num_epochs $NUM_EPOCHS --batch_size 1024 \
+   --N_importance 256 --img_wh $IMG_W $IMG_H \
+   --spheric_poses --use_disp \
+   --num_epochs $NUM_EPOCHS --batch_size 2048 \
    --optimizer adam --lr 5e-4 \
    --lr_scheduler cosine \
    --exp_name $EXP
