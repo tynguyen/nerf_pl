0bd8529796c9a8cbac9753dac44abe09955d5bf0
Ty Nguyen <t.nguyen5@partner.samsung.com>
2021-07-19 15:51:34 -0400

    Fix the eval shell script

--- scripts/eval_replica_room_0.sh
+++ scripts/eval_replica_room_0.sh
@@ -1,4 +1,15 @@
+# directory containing the data
+ROOT_DIR="data/replica/room_0"
+EXP_DIR="ckpts"
+IMG_W=640  # image width (do not set too large)
+IMG_H=360  # image height (do not set too large)
+
+EXP="exp_replica_room_0_spheric"  # name of the experience (arbitrary)
+EPOCH="3"  # name of the experience (arbitrary)
+
+
 python eval.py \
-   --root_dir data/replica/room_0\
+   --root_dir "$ROOT_DIR" \
    --dataset_name llff --scene_name room_0 \
-   --img_wh 640 360 --N_importance 64 --ckpt_path 'ckpts/exp_replica_room_0/epoch=2.ckpt' \
+   --spheric_poses --use_disp \
+   --img_wh ${IMG_W} ${IMG_H} --N_importance 256 --ckpt_path ${EXP_DIR}/${EXP}/epoch=${EPOCH}.ckpt \
